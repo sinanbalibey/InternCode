1ac4f4a868fc2434d810be17e52b02e3d4b00c47
Sinan Balıbey <sinanbey23@hotmail.com>
2023-07-09 14:19:57 +0300

    ArrayMethodExample

---
+++ InternDay1/Inter1-1/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Dizi dizi=new Dizi();
+
+        dizi.Add(12);
+        dizi.Add(13);
+        dizi.Add(14);
+        dizi.Add(15);
+        dizi.Add(13);
+        dizi.Add(10);
+
+        dizi.Print();
+
+        dizi.Sil(10);
+        dizi.Print();
+
+
+
+    }
+}
