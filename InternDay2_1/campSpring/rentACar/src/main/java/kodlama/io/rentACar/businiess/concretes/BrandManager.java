3f24f39a143dab329abcad587e7f14e1700928fc
Sinan Balıbey <sinanbey23@hotmail.com>
2023-07-10 15:05:13 +0300

    Spring Giris Proje

---
+++ InternDay2_1/campSpring/rentACar/src/main/java/kodlama/io/rentACar/businiess/concretes/BrandManager.java
@@ -0,0 +1,33 @@
+package kodlama.io.rentACar.businiess.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.rentACar.businiess.abstracts.BrandService;
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@Service//Bu sınıf bir business nesnesidir.
+public class BrandManager implements BrandService {
+
+	private  BrandRepository brandRepository;
+	
+	@Autowired
+	public BrandManager(BrandRepository brandRepository) {
+		this.brandRepository = brandRepository;
+	}
+
+
+
+
+	@Override
+	public List<Brand> getAll() {
+		//iş kuralları
+		
+		return brandRepository.getAll();
+	}
+	
+
+}
