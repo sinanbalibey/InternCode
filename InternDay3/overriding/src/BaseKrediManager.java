f3b235dd5c330d88f2c61c445a206631690baaf6
Sinan <75623326+sinanbalibey@users.noreply.github.com>
2023-07-10 09:57:20 +0300

    Delete InternDay3 directory

--- InternDay3/overriding/src/BaseKrediManager.java
+++
@@ -1,9 +0,0 @@
-public class BaseKrediManager {
-    public  double hesapla(double tutar){
-
-        return  tutar*1.18;
-    }
-}
-//Bazı durumlarda ezmeye izin vermediğimiz zamanlar oluyor
-//Bu durumda ise public final double hesapla(double tutar)
-//Şeklinde olur.
