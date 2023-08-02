43aacd362e71587c57ef16fb63c621adc39c3f2e
Sinan Balıbey <sinanbey23@hotmail.com>
2023-08-02 21:25:40 +0300

    htmlCssEklendi

--- InternDay3_4/boxCase3-4/src/main/java/com/BoxCase34/boxCase34/controller/AppController.java
+++ InternDay3_4/boxCase3-4/src/main/java/com/BoxCase34/boxCase34/controller/AppController.java
@@ -1,13 +1,16 @@
 package com.BoxCase34.boxCase34.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
-public class HomeController {
-	@GetMapping("/girisYap")
-    public String girisYap() {
-        
-        return "girisYap.html";
-    }
+public class AppController {
+
+	@GetMapping("")
+	public String viewHomePage() {
+		return "anasayfa";
+	}
 }
