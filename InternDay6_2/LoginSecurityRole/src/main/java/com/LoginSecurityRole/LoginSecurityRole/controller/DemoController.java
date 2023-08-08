26e7e1723cdd1b763ee64e4031b745343acf5128
Sinan Balıbey <sinanbey23@hotmail.com>
2023-08-08 19:56:58 +0300

    InMemoryLoginAndRoles

--- InternDay6_2/LoginSecurityRole/src/main/java/com/LoginSecurityRole/LoginSecurityRole/controller/DemoController.java
+++ InternDay6_2/LoginSecurityRole/src/main/java/com/LoginSecurityRole/LoginSecurityRole/controller/DemoController.java
@@ -1,2 +1,23 @@
-package com.LoginSecurityRole.LoginSecurityRole.controller;public class DemoController {
+package com.LoginSecurityRole.LoginSecurityRole.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+    //reques mapping for==>leaders
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+    //sadece adminler için
+    @GetMapping("/systems")
+    public String showSystems(){
+        return "systems";
+    }
 }
--- InternDay6_2/LoginSecurityRole/src/main/java/com/LoginSecurityRole/LoginSecurityRole/controller/LoginController.java
+++ InternDay6_2/LoginSecurityRole/src/main/java/com/LoginSecurityRole/LoginSecurityRole/controller/LoginController.java
@@ -1,2 +1,21 @@
-package com.LoginSecurityRole.LoginSecurityRole.controller;public class LoginController {
+package com.LoginSecurityRole.LoginSecurityRole.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+       // return  "plain-login";
+        return "fancy-login";
+    }
+
+    //add reguestmapping accesdeniend(hata için)
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+        // return  "plain-login";
+        return "access-denied";
+    }
 }
